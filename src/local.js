1b32c8f11e58a5fb4a288192227ec9e241c3773e
Andrey Lukin <aimm.legate@gmail.com>
2018-05-04 23:59:44 +0500

    fix linter

--- src/local.js
+++ src/local.js
@@ -1,7 +1,4 @@
-export const toStorage = (data) => {
-  localStorage.setItem('aimmlRss', JSON.stringify(data));
-};
+export const toStorage = data => localStorage.setItem('aimmlRss', JSON.stringify(data));
 
-export const getFromStorage = () => {
-  return JSON.parse(localStorage.getItem('aimmlRss'));
-};
+export const getFromStorage = () => JSON.parse(localStorage.getItem('aimmlRss'));
+
